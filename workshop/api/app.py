fd53998458b86ffe5b44d4dfb222c7d4e0acb9de
Krypton-gamma-kplr <130390426+Krypton-gamma-kplr@users.noreply.github.com>
2023-06-14 10:50:56 +0000

    avancé sur le workshop

--- workshop/api/app.py
+++ workshop/api/app.py
@@ -1,17 +1,17 @@
 from flask import Flask 
 import psycopg2
-
+from flask import jsonify
+from flask import request
 # Création de l'objet Flask
 app = Flask(__name__)
 
 # Configuration de la connexion à la base de données
 conn = psycopg2.connect(
     host='horton.db.elephantsql.com',
-    database='mydata',
+    database='zasnrhbz',
     user='zasnrhbz',
     password='c_2dQQfWMWhCHkuxpP9l5ZB0cVZ4Ed5K',
     port='5432' ,
-    sslmode='require'
 )
 
 
@@ -21,7 +21,57 @@ def get_data():
     cur.execute('SELECT * FROM data')
     rows = cur.fetchall()
     cur.close()
-    return str(rows)
+
+    return jsonify(rows)
+
+@app.route('/data/<int:id>')
+def get_data_by_id(id):
+    cur = conn.cursor()
+    cur.execute(f'SELECT * FROM data WHERE id ={id}')
+    rows = cur.fetchone()
+    cur.close()
+
+    return jsonify(rows)
+
+@app.route('/add_data_form')
+def add_data_form():
+    return open('/workspaces/Flask-webapi-gamma/workshop/api/form.html').read()
+
+@app.route('/add_data', methods=['GET','POST'])
+def add_data():
+    cur = conn.cursor()
+    id = request.form['id']
+    name = request.form['name']
+    value = request.form['value']
+
+    cur.execute("INSERT INTO data (id, name, value) VALUES (%s, %s, %s)", (id, name, value))
+    conn.commit()
+    cur.close()
+
+    return jsonify('Data added successfully')
+
+@app.route('/maj_data_maj')
+def maj_data_maj():
+    return open('/workspaces/Flask-webapi-gamma/workshop/api/maj.html').read()
+@app.route('/maj_data', methods=['POST'])
+def maj_data():
+    cur = conn.cursor()
+    id = request.form['id']
+    name = request.form['name']
+    value = request.form['value']
+
+    if request.form['_method'] == 'PUT':
+        cur.execute("UPDATE data SET name = %s, value = %s WHERE id = %s", (name, value, id))
+        conn.commit()
+        cur.close()
+
+        return jsonify('Data updated successfully')
+    else:
+        return 'Invalid request'
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
